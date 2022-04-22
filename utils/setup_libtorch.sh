64e2b7f55454cd17215f94744e89c4cee4b0e3c2
dophist <jerry.ejwt@qq.com>
2022-04-22 21:11:40 +0800

    add doc comment to utils/setup_libtorch.sh

--- utils/setup_libtorch.sh
+++ utils/setup_libtorch.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# document
+#   https://pytorch.org/cppdocs/installing.html
 cd deps
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
 cd -
 
 echo "Done"
+
