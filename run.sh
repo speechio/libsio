8dbcdcce50546d7f58c261924ff521056ce59bba
dophist <jerry.jiayu.du@gmail.com>
2022-04-23 00:39:13 +0800

    fix rpath loading on Mac

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ rm -f build/stt
 cmake -S . -B build
 cmake --build build -j 40
 
+if [[ `uname -s` == "Darwin" ]]; then
+    export DYLD_LIBRARY_PATH=/opt/intel/mkl/lib:deps/kaldi/dist/lib
+fi
+
 SIO_VERBOSITY=INFO build/unittest
 SIO_VERBOSITY=INFO build/stt
 
