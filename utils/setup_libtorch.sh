17bb7fd667d25e8e2095eb14f2a9def4bf134a4b
dophist <jerry.jiayu.du@gmail.com>
2022-04-22 23:50:04 +0800

    add libtorch support for Mac

--- utils/setup_libtorch.sh
+++ utils/setup_libtorch.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-# document: https://pytorch.org/cppdocs/installing.html
+# get download link from webpage:
+#   https://pytorch.org/
+#
+# installation note:
+#   https://pytorch.org/cppdocs/installing.html
+
 cd deps
-wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
-unzip libtorch-shared-with-deps-latest.zip
+
+if [[ `uname -s` == "Linux" ]]; then
+    wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+    unzip libtorch-shared-with-deps-latest.zip
+elif [[ `uname -s` == "Darwin" ]]; then
+    wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-macos-latest.zip
+    unzip libtorch-macos-latest.zip
+fi
+
 cd -
 
 echo "Done"
