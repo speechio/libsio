a5d3c30662e1c2606027f840f59e37868faab39e
dophist <jerry.ejwt@qq.com>
2022-04-22 21:25:49 +0800

    minor

--- utils/setup_libtorch.sh
+++ utils/setup_libtorch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# document
-#   https://pytorch.org/cppdocs/installing.html
+# document: https://pytorch.org/cppdocs/installing.html
 cd deps
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
