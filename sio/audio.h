5f9a6ee206d7e4be0ffb386d13df84e211b0a48f
dophist <jerry.jiayu.du@gmail.com>
2022-04-23 20:50:27 +0800

    make libsio header-only, and clean CMakeLists.txt

--- sio/audio.h
+++ sio/audio.h
@@ -1,11 +1,36 @@
 #ifndef SIO_AUDIO_H
 #define SIO_AUDIO_H
 
+#include <fstream>
+#include <feat/wave-reader.h> // kaldi
 #include "sio/base.h"
 
 namespace sio {
 
-Error ReadAudio(const Str& filepath, Vec<f32>* samples, f32* sample_rate);
+inline Error ReadAudio(const Str& filepath, Vec<f32>* samples, f32* sample_rate) {
+    std::ifstream is(filepath, std::ifstream::binary);
+	if (!is.good()) {
+        return Error::OpenFileFailure;
+    }
+
+    kaldi::WaveData kaldi_wave;
+    kaldi_wave.Read(is);
+
+    *sample_rate = kaldi_wave.SampFreq();
+    kaldi::SubVector<f32> ch0(kaldi_wave.Data(), 0);
+
+    SIO_CHECK(samples != nullptr);
+    if (!samples->empty()) {
+        samples->clear();
+    }
+    samples->resize(ch0.Dim(), 0.0f);
+
+    for (int i = 0; i < ch0.Dim(); i++) {
+        (*samples)[i] = ch0(i);
+    }
+
+    return Error::OK;
+}
 
 }  // namespace sio
 #endif
--- sio/error.h
+++ sio/error.h
@@ -1,6 +1,9 @@
 #ifndef SIO_ERROR_H
 #define SIO_ERROR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "sio/macro.h"
 
 namespace sio {
@@ -15,9 +18,27 @@ enum class Error : int {
     Unknown,
 }; // enum class Error
 
-const char* ErrorMsg(Error err);
 
-void Panic(const char* file, int line, const char* func, Error err);
+inline const char* ErrorMsg(Error err) {
+    switch (err) {
+        case Error::OK: return "(OK)";
+        case Error::OutOfMemory: return "out of memory";
+        case Error::AssertionFailure: return "assertion failure";
+        case Error::OpenFileFailure: return "cannot open file";
+        case Error::VocabularyMismatch: return "mismatched vocabulary of tokenizer and KenLM";
+        case Error::NoRecognitionResult: return "no recognition result";
+        case Error::Unknown: return "(unknown error)";
+    }
+
+    return nullptr; /* avoid warning */
+}
+
+
+inline void Panic(const char* file, int line, const char* func, Error err) {
+    fprintf(stderr, "[panic](%s:%d:%s) %s\n", file, line, func, ErrorMsg(err));
+    fflush(stderr);
+    abort();
+}
 
 #define SIO_PANIC(err) ::sio::Panic(SIO_FILE_REPR, SIO_LINE_REPR, SIO_FUNC_REPR, err)
 
