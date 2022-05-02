f2f3bbda21ddd4cb0386664a4d331f45f7ab8b39
dophist <jerry.jiayu.du@gmail.com>
2022-05-02 18:51:47 +0800

    fix constness in sio header

--- src/sio.h
+++ src/sio.h
@@ -14,7 +14,7 @@ int sio_destroy(struct sio_module* m);
 struct sio_stt* sio_stt_create(struct sio_module* m);
 int sio_stt_destroy(struct sio_stt* stt);
 
-int sio_stt_speech(struct sio_stt* stt, float* samples, int n, float sample_rate);
+int sio_stt_speech(struct sio_stt* stt, const float* samples, int n, float sample_rate);
 int sio_stt_to(struct sio_stt* stt);
 const char* sio_stt_text(struct sio_stt* stt);
 int sio_stt_reset(struct sio_stt* stt);
