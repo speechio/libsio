264049945d2830ac981b74bc6a63171eedb85e9b
dophist <jerry.jiayu.du@gmail.com>
2022-05-04 13:48:12 +0800

    simplify sio capi naming

--- src/sio.cc
+++ src/sio.cc
@@ -3,42 +3,42 @@
 #include "sio/stt.h"
 //#include "sio/tts.h"
 
-struct sio_handle sio_create(const char* path) {
+struct sio_module sio_create(const char* path) {
     sio::SpeechToTextModel* p = new sio::SpeechToTextModel;
     p->Load(path);
     return { (uintptr_t)p, (uintptr_t)nullptr };
 }
 
-int sio_destroy(struct sio_handle sio) {
-    delete (sio::SpeechToTextModel*)sio.stt_module;
-    //delete (sio::TextToSpeechModel*)sio.tts_module;
+int sio_destroy(struct sio_module m) {
+    delete (sio::SpeechToTextModel*)m.stt_module;
+    //delete (sio::TextToSpeechModel*)m.tts_module;
     return 0;
 }
 
-struct sio_stt_handle sio_stt_create(struct sio_handle sio) {
+struct sio_stt sio_stt_create(struct sio_module m) {
     sio::SpeechToText* p = new sio::SpeechToText;
-    p->Load(*(sio::SpeechToTextModel*)sio.stt_module);
+    p->Load(*(sio::SpeechToTextModel*)m.stt_module);
     return { (uintptr_t)p };
 }
 
-int sio_stt_destroy(struct sio_stt_handle stt) {
+int sio_stt_destroy(struct sio_stt stt) {
     delete (sio::SpeechToText*)stt.runtime;
     return 0;
 }
 
-int sio_stt_speech(struct sio_stt_handle stt, const float* samples, int n, float sample_rate) {
+int sio_stt_speech(struct sio_stt stt, const float* samples, int n, float sample_rate) {
     return ((sio::SpeechToText*)stt.runtime)->Speech(samples, n, sample_rate);
 }
 
-int sio_stt_to(struct sio_stt_handle stt) {
+int sio_stt_to(struct sio_stt stt) {
     return ((sio::SpeechToText*)stt.runtime)->To();
 }
 
-const char* sio_stt_text(struct sio_stt_handle stt) {
+const char* sio_stt_text(struct sio_stt stt) {
     return ((sio::SpeechToText*)stt.runtime)->Text();
 }
 
-int sio_stt_reset(struct sio_stt_handle stt) {
+int sio_stt_reset(struct sio_stt stt) {
     return ((sio::SpeechToText*)stt.runtime)->Reset();
 }
 
--- src/sio.h
+++ src/sio.h
@@ -7,26 +7,25 @@
 extern "C" {
 #endif
 
-struct sio_handle {
+struct sio_module {
 	uintptr_t stt_module;
 	uintptr_t tts_module;
 };
 
-struct sio_handle sio_create(const char* path);
-int sio_destroy(struct sio_handle sio);
-
-
-struct sio_stt_handle {
+struct sio_stt {
 	uintptr_t runtime;
 };
 
-struct sio_stt_handle sio_stt_create(struct sio_handle sio);
-int sio_stt_destroy(struct sio_stt_handle stt);
+struct sio_module sio_create(const char* filepath);
+int sio_destroy(struct sio_module);
 
-int sio_stt_speech(struct sio_stt_handle stt, const float* samples, int n, float sample_rate);
-int sio_stt_to(struct sio_stt_handle stt);
-const char* sio_stt_text(struct sio_stt_handle stt);
-int sio_stt_reset(struct sio_stt_handle stt);
+struct sio_stt sio_stt_create(struct sio_module);
+int sio_stt_destroy(struct sio_stt);
+
+int sio_stt_speech(struct sio_stt, const float* samples, int n, float sample_rate);
+int sio_stt_to(struct sio_stt);
+const char* sio_stt_text(struct sio_stt);
+int sio_stt_reset(struct sio_stt);
 
 #ifdef __cplusplus
 }
